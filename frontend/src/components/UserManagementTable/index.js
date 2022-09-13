08c708a68d2f77bf56f0dc8cb5f023838beb58c3
zwei8850 <zizhengwei@gmail.com>
2022-09-14 01:19:02 +1000

    Update Admin UserManagement Page to the front end

---
+++ frontend/src/components/UserManagementTable/index.js
@@ -0,0 +1,85 @@
+import { Space, Table, Tag } from 'antd';
+import React, { useState } from 'react';
+
+// table header
+const columns = [
+  {
+    title: 'ID',
+    dataIndex: 'id',
+    defaultSortOrder: 'ascend',
+    sorter: (a, b) => a.id - b.id,
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+  },
+  {
+    title: 'Tags',
+    dataIndex: 'tags',
+    render: (_, { tags }) => (
+      <>
+        {tags.map((tag) => {
+          let color = 'green';
+          if (tag === 'gym owner') {
+            color = 'volcano';
+          }
+
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'operation',
+    fixed: 'right',
+    width: 100,
+    render: (_, record) => (
+      <Space size="large">
+        <a>Invite</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+];
+
+// generate user list
+const data1 = [];
+let len = 50;
+while(len--) {
+  data1.unshift({
+    id: len,
+    key: len,
+    name: 'user' + len,
+    email: 'test' + len +' @gmail.com',
+    tags: ['normal user'],
+  })
+}
+
+// function
+function UserManagementTable () {
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const onSelectChange = (newSelectedRowKeys) => {
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
+
+  // row selection can be found at the top left of the table
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+  };
+
+  return <Table dataSource={data1} columns={columns} rowSelection={rowSelection} pagination={{ pageSize: 8 }}/> ;
+  
+  
+}
+
+export default UserManagementTable;
--- frontend/src/pages/UsersManagementPage/UsersManagementPage.js
+++ frontend/src/pages/UsersManagementPage/UsersManagementPage.js
@@ -1,4 +1,21 @@
-const UserManagementPage = () => {
-  return <>user UserManagementPage</>;
-};
+import React from 'react';
+import { Layout } from 'antd';
+import Table from '../../components/UserManagementTable';
+import "./UserManagementPage.scss"
+
+const { Content} = Layout;
+
+
+function UserManagementPage() {
+  return(
+    <Layout>
+      <Layout className="header">
+        <h2>User Table</h2>  
+      </Layout>
+      <Content className="content">
+          <Table />
+      </Content>
+    </Layout>
+  );
+}
 export default UserManagementPage;
