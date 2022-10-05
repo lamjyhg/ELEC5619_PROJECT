204863811734be9f50b4ed8e9a684699bee44d29
zizheng wei <zizhengwei@gmail.com>
2022-10-05 16:01:22 +1100

    Single Sym Request page update

--- frontend/src/pages/GymRequestPage/GymRequestPage.js
+++ frontend/src/pages/GymRequestPage/GymRequestPage.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Layout } from 'antd';
 import {useParams} from "react-router-dom";
+import SingleGymView from "../../components/AdminTable/SingleGymView/SingleGymView";
 
 function GymRequestPage(){
   const { id } = useParams();
   return (
-      <>gym request page for id: {id}</>
+      <Layout>
+        <p>gym request page for id: {id}</p>
+        <SingleGymView GID={id}/>
+      </Layout>
   );
 };
 export default GymRequestPage;
