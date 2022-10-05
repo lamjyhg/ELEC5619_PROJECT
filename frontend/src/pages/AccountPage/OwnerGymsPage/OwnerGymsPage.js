6bf41feff4898b35cba193f4a4919ffc13c7f721
ming25 <ming.xie@everty.com.au>
2022-10-05 20:06:58 +1100

    update create gym

--- frontend/src/pages/AccountPage/OwnerGymsPage/OwnerGymsPage.js
+++ frontend/src/pages/AccountPage/OwnerGymsPage/OwnerGymsPage.js
@@ -1,74 +1,159 @@
-import {
-  LoadingOutlined,
-  PlusOutlined,
-  UploadOutlined,
-} from "@ant-design/icons";
+import { UploadOutlined } from "@ant-design/icons";
 import { Button, Form, Input, InputNumber, Modal, Upload } from "antd";
 import { useState } from "react";
 import { geocodeByAddress, getLatLng } from "react-places-autocomplete";
 import AddressAutoComplete from "../../../components/AddressAutoComplete/AddressAutoComplete";
 import "./OwnerGymsPage.scss";
+const { TextArea } = Input;
 
-const OwnerGymsPage = () => {
-  const [loading, setLoading] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [formValue, setFormValue] = useState({
-    name: "",
-    address: "",
-    maximumOfAppointments: "",
-    imageUrl: "",
-    geoLocation: { lat: "", lng: "" },
-  });
-  const onFinish = (values) => {
-    console.log("Success:", values);
-  };
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleOk = () => {
-    // submit gym
-    setIsModalOpen(false);
-  };
-
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
-
-  const normFile = (e) => {
-    console.log("Upload event:", e);
-    if (Array.isArray(e)) {
-      return e;
-    }
-    return e?.fileList;
-  };
+const GymForm = ({ open, onCreate, onCancel, acitonType }) => {
+  const [form] = Form.useForm();
+  const [address, setAddress] = useState("");
   const clearAddress = () => {
-    // set address empty filed with antd form
-    setFormValue((previousForm) => ({ ...previousForm, address: "" }));
+    setAddress("");
   };
   const handleAddressSelect = (address, placeID) => {
-    console.log({ address, placeID });
+    form.setFieldValue("address", address);
     geocodeByAddress(address)
       .then(async (results) => {
-        // Do something with results[0]
         return getLatLng(results[0]);
       })
       .then((latLng) => {
-        // Do something with latLng
-        setFormValue((previousForm) => ({
-          ...previousForm,
-          address,
-          geoLocation: { lat: latLng.lat, lng: latLng.lng },
-        }));
+        setAddress(address);
+        form.setFieldValue("geoLocation", {
+          lat: latLng.lat,
+          lng: latLng.lng,
+        });
       })
       .catch((error) => {
         console.error("Error", error);
       });
   };
-  const handleAddressChange = (address) => {
+  const handleAddressChange = ({ address }) => {
     // set form address value
-    console.log({ address });
-    setFormValue((previousForm) => ({ ...previousForm, address }));
+    form.setFieldValue("address", address);
+  };
+  const normFile = (e) => {
+    console.log("Upload event:", e);
+    // form.setFieldsValue("imageUrl", e.file.name);
+    //set imgae url
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e?.fileList;
+  };
+
+  return (
+    <Modal
+      visible={open}
+      title={acitonType === "CREATE" ? "Create Gym" : "Update Gym"}
+      okText={acitonType === "CREATE" ? "Create Gym" : "Update Gym"}
+      cancelText="Cancel"
+      onCancel={() => {
+        onCancel();
+        setAddress("");
+      }}
+      onOk={() => {
+        form
+          .validateFields()
+          .then((values) => {
+            console.log({ values });
+            form.resetFields();
+            setAddress("");
+            onCreate(values);
+          })
+          .catch((info) => {
+            console.log("Validate Failed:", info);
+          });
+      }}
+    >
+      <Form
+        form={form}
+        labelCol={{ span: 7 }}
+        wrapperCol={{ span: 15 }}
+        layout="horizontal"
+      >
+        <Form.Item
+          rules={[
+            {
+              required: true,
+              message: "Please input your gym name!",
+            },
+          ]}
+          label="Gym name"
+          name="name"
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Address"
+          name="address"
+          rules={[
+            {
+              required: true,
+              message: "Please input your gym name!",
+            },
+          ]}
+          onChange={handleAddressSelect}
+        >
+          <AddressAutoComplete
+            address={address}
+            clearAddress={clearAddress}
+            onChange={handleAddressChange}
+            onAddressSelect={handleAddressSelect}
+          />
+        </Form.Item>
+        <Form.Item hidden name="geoLocation"></Form.Item>
+        <Form.Item
+          label="Max Appointment"
+          name="maximumOfAppointments"
+          rules={[
+            {
+              required: true,
+              message: "Please input your gym name!",
+            },
+          ]}
+        >
+          <Form.Item name="maximumOfAppointments" noStyle>
+            <InputNumber name="maximumOfAppointments" min={0} max={100} />
+          </Form.Item>
+        </Form.Item>
+        <Form.Item
+          label="Description"
+          name="description"
+          rules={[
+            {
+              required: true,
+              message: "Please input your gym name!",
+            },
+          ]}
+        >
+          <TextArea name="description" />
+        </Form.Item>
+        <Form.Item
+          label="Cover Image"
+          name="imageUrl"
+          valuePropName="fileList"
+          getValueFromEvent={normFile}
+        >
+          <Upload action="/upload.do" listType="picture" maxCount={1}>
+            <Button icon={<UploadOutlined />}>Click to upload</Button>
+          </Upload>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+const OwnerGymsPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const onCreate = (values) => {
+    console.log("Received values of form: ", values);
+    setIsModalOpen(false);
   };
 
   return (
@@ -77,52 +162,14 @@ const OwnerGymsPage = () => {
       <Button type="primary" onClick={showModal}>
         Create gym
       </Button>
-      {/* reuse this modal for update gym */}
-      <Modal
-        title="Create Gym"
-        visible={isModalOpen}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
-        <Form
-          onFinish={onFinish}
-          labelCol={{ span: 7 }}
-          wrapperCol={{ span: 15 }}
-          layout="horizontal"
-        >
-          <Form.Item label="Gym name">
-            <Input />
-          </Form.Item>
-          <Form.Item label="Address">
-            <AddressAutoComplete
-              address={formValue.address}
-              clearAddress={clearAddress}
-              onChange={handleAddressChange}
-              onAddressSelect={handleAddressSelect}
-            />
-          </Form.Item>
-          <Form.Item label="Max Appointment">
-            <Form.Item name="input-number" noStyle>
-              <InputNumber name="maximumOfAppointments" min={0} max={100} />
-            </Form.Item>
-          </Form.Item>
-          <Form.Item
-            label="Cover Image"
-            name="upload"
-            valuePropName="fileList"
-            getValueFromEvent={normFile}
-          >
-            <Upload
-              name="imageUrl"
-              action="/upload.do"
-              listType="picture"
-              maxCount={1}
-            >
-              <Button icon={<UploadOutlined />}>Click to upload</Button>
-            </Upload>
-          </Form.Item>
-        </Form>
-      </Modal>
+      <GymForm
+        open={isModalOpen}
+        onCreate={onCreate}
+        acitonType={"CREATE"}
+        onCancel={() => {
+          setIsModalOpen(false);
+        }}
+      />
     </div>
   );
 };
